43bb2d4e5aee420748369e518a6effca0fbdffbe
Quantumzhao <quantumzhao@protonmail.ch>
2023-03-31 23:02:01 -0400

    smiling Harold.jpg

--- Decompressor/GZTool/Conventions.cs
+++ Decompressor/GZTool/Conventions.cs
@@ -1,4 +1,6 @@
 
+using System.Diagnostics;
+
 namespace ParallelParsing.GZTool;
 
 public record class Point(
@@ -13,20 +15,90 @@ public record class Point(
 	// size of (compressed) window
 	uint WindowSize,
 	// preceding 32K of uncompressed data, compressed
-	char[] Window
+	SChar[] Window,
+	ulong lineNumber
 );
 
-public record class Index(
-	ulong Have, 
-	ulong Size,
-	ulong FileSize,
-	Point[] List,
-	string FileName,
-	bool IsIndexComplete
-	/* index_version should be 0 (default), thus omitted */
-	/* line_number_format should be irrelevant if index contains no info about line number */
-	/* number_of_lines omitted, for the same reason */
-);
+// public class Index
+// {
+// 	public ulong FileSize;
+// 	public List<Point> List;
+// 	public string? FileName;
+// 	public bool IsIndexComplete;
+// 	/* index_version should be 0 (default), thus omitted */
+// 	/* line_number_format should be irrelevant if index contains no info about line number */
+// 	/* number_of_lines omitted, for the same reason */
+
+// 	public Index AddPoint(uint bits, ulong input, ulong output, uint left, 
+// 		ref SChar window, uint windowSize, ulong lineNumber, bool isCompressedWindow)
+// 	{
+// 		// Point next;
+// 		// ulong size = windowSize;
+
+// 		// if (isCompressedWindow)
+// 		// {
+// 		// 	next = new Point {
+// 		// 		Output = output,
+// 		// 		Input = input,
+// 		// 		Bits = bits,
+
+// 		// 	}
+
+// 		// }
+// 		// else
+// 		// {
+
+// 		// }
+
+// 		// List.Add(next);
+
+// 		return this;
+// 	}
+
+// 	public Index()
+// 	{
+// 		FileSize = 0;
+// 		List = new List<Point>(8);
+// 		IsIndexComplete = false;
+// 		FileName = null;
+// 	}
+
+// 	// private unsafe SChar[]? CompressChunk(ref SChar source, ref ulong size, int level)
+// 	// {
+// 	// 	ZSignal flush;
+// 	// 	ZResult ret;
+// 	// 	uint have;
+// 	// 	ulong i = 0;
+// 	// 	ulong outputSize = 0;
+// 	// 	ulong inputSize = size;
+// 	// 	SChar[] input, output, outComplete;
+
+// 	// 	if (Defined.DeflateInit(out var strm, level) is not ZResult.OK) return null;
+
+// 	// 	input = new SChar[Constants.CHUNK];
+// 	// 	output = new SChar[Constants.CHUNK];
+// 	// 	outComplete = new SChar[Constants.CHUNK];
+
+// 	// 	do
+// 	// 	{
+// 	// 		strm.Value.avail_in = (uint)(i + Constants.CHUNK < inputSize ? 
+// 	// 									Constants.CHUNK : 
+// 	// 									inputSize - i);
+
+// 	// 		do
+// 	// 		{
+// 	// 			strm.NextOut = output;
+// 	// 		} while (true);
+			
+// 	// 	} while (flush != ZSignal.FINISH);
+
+// 	// 	if (ret != ZResult.STREAM_END) throw new UnreachableException();
+
+// 	// 	Defined.DeflateEnd(strm);
+// 	// 	size = outputSize;
+// 	// 	return outComplete;
+// 	// }
+// }
 
 public enum IndexAndExtractionOptions
 {
@@ -67,11 +139,71 @@ public enum DecompressInAdvanceInitializers
 	SoftReset
 }
 
+public enum ZResult
+{
+	OK = 0,
+	STREAM_END = 1,
+	NEED_DICT = 2,
+	ERRNO = -1,
+	STREAM_ERROR = -2,
+	DATA_ERROR = -3,
+	MEM_ERROR = -4,
+	BUF_ERROR = -5,
+	VERSION_ERROR = -6
+}
+
+public enum ZSignal
+{
+	NO_FLUSH,
+	PARTIAL_FLUSH,
+	SYNC_FLUSH,
+	FULL_FLUSH,
+	FINISH,
+	BLOCK,
+	TREES
+}
+
 public static class Constants
 {
 	public const int MAX_GIVE_ME_SI_UNIT_RETURNED_LENGTH = 14;
 	[Obsolete]
 	public static char[] number_output = new char[MAX_GIVE_ME_SI_UNIT_RETURNED_LENGTH]; 
+	
+	// desired distance between access points
+	public const long SPAN = 10485760L;
+
+	// sliding window size
+	public const uint WINSIZE = 32768U;
+
+	// file input buffer size
+	public const uint CHUNK = 16384;
+
+	// window is an uncompressed WINSIZE size window
+	public const uint UNCOMPRESSED_WINDOW = uint.MaxValue;
+
+	// default index version (v0)
+	public const string GZIP_INDEX_IDENTIFIER_STRING = "gzipindx";
+
+	// index version with line number info
+	public const string GZIP_INDEX_IDENTIFIER_STRING_V1 = "gzipindeX";
+
+	// header size in bytes of gztool's .gzi files
+	public const int GZIP_INDEX_HEADER_SIZE = 16;
+
+	// header size in bytes of gzip files created by zlib
+	// github.com/madler/zlib/blob/master/zlib.h
+	public const int GZIP_INDEX_SIZE_BY_ZLIB = 10;
+
+	// If deflateSetHeader is not used, the default gzip header has text false,
+	// the time set to zero, and os set to 255, with no extra, name, or comment fields.
+	// default waiting time in seconds when supervising a growing gzip file:
+	public const int WAITING_TIME = 4;
+
+	// how many CHUNKs will be decompressed in advance if it is needed (parameter gzip_stream_may_be_damaged, `-p`)
+	public const int CHUNKS_TO_DECOMPRESS_IN_ADVANCE = 3;
+
+	// how many CHUNKs will be look to backwards for a new good gzip reentry point after an error is found (with `-p`)
+	public const int CHUNKS_TO_LOOK_BACKWARDS = 3;
 }
 
 [Obsolete]
--- Decompressor/GZTool/PlatformInterop.cs
+++ Decompressor/GZTool/PlatformInterop.cs
@@ -1,5 +1,5 @@
-
-using System.IO.Compression;
+using System.IO;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -56,33 +56,76 @@ public static class ExternalCalls
 [StructLayout(LayoutKind.Sequential)]
 public unsafe struct z_stream
 {
-	public byte* next_int;
+	public SChar* next_in;
 	public uint avail_in;
 	public ulong total_in;
-	public byte* next_out;
+	public SChar* next_out;
 	public uint avail_out;
 	public ulong total_out;
 	public SChar* msg;
 	public void* state;
 	// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/proposals/csharp-9.0/function-pointers
-	public delegate* unmanaged [Cdecl]<void*, uint, uint, void*> zalloc;
-	public delegate* unmanaged [Cdecl]<void*, void*, void> zfree;
+	// unused
+	public delegate* unmanaged[Cdecl]<void*, uint, uint, void*> zalloc;
+	// unused
+	public delegate* unmanaged[Cdecl]<void*, void*, void> zfree;
+	// unused
 	public void* opaque;
 	public int data_type;
+	// unused
 	public ulong adler;
 	public ulong reserved;
+}
+
+public class ZStream
+{
+	public unsafe ZStream(z_stream* strm)
+	{
+		_Value = *strm;
+		NextIn = new UnmanagedArray<SChar>(_Value.next_in, _Value.avail_in);
+		NextOut = new UnmanagedArray<SChar>(_Value.next_out, _Value.avail_out);
+		Message = Marshal.PtrToStringAnsi((IntPtr)_Value.msg);
+	}
+
+	[FixedAddressValueType]
+	private z_stream _Value;
+
+	public UnmanagedArray<SChar> NextIn;
+	public ulong TotalIn => _Value.total_in;
+	public UnmanagedArray<SChar> NextOut;
+	public ulong TotalOut => _Value.total_out;
+	public string? Message;
+	public int DataType => _Value.data_type;
+
+	public static unsafe implicit operator z_stream*(ZStream strm)
+	{
+		fixed (z_stream* ptr = &strm._Value)
+		{
+			return ptr;
+		}
+	}
 }
 
 public static class Defined
 {
 	private const string ZLIB_VERSION = "1.2.11";
 	
-	public static unsafe int deflateInit(z_stream* strm, int level)
+	public unsafe static ZResult DeflateInit(out ZStream strm, int level)
 	{
 		fixed (char* s = ZLIB_VERSION)
 		{
-			return ExternalCalls.deflateInit_(strm, level, s, (int)sizeof(z_stream));
+			z_stream* ret = default;
+			var res = (ZResult)ExternalCalls.deflateInit_(ret, level, s, (int)sizeof(z_stream));
+			strm = new ZStream(ret);
+			return res;
 		}
+	}
+
+	public unsafe static ZResult DeflateEnd(in ZStream strm)
+	{
+		z_stream* res = default;
+		var ret = (ZResult)ExternalCalls.deflateEnd((z_stream*)strm);
+		return ret;
 	}
 }
 
@@ -99,4 +142,31 @@ public struct SChar
 public class UnmanagedString
 {
 
+}
+
+public unsafe class UnmanagedArray<T> where T : unmanaged
+{
+	public UnmanagedArray(T* ptr, uint size)
+	{
+		_Ptr = ptr;
+		Length = size;
+	}
+
+	public readonly uint Length;
+	private readonly T* _Ptr;
+
+	public T this[int index]
+	{
+		get
+		{
+			if (index < Length) return *(_Ptr + index);
+			else throw new IndexOutOfRangeException();
+		}
+
+		set
+		{
+			if (index < Length) *(_Ptr + index) = value;
+			else throw new IndexOutOfRangeException();
+		}
+	}
 }
